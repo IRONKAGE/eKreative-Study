13e17bd3ef802debc45e1794624d19984e68df1b
Oleh Hatsenko <40500897+IRONKAGE@users.noreply.github.com>
2019-11-04 20:27:43 +0200

    Add files via upload
    Підкорегував та добавив рішення 6.4

--- Node.JS/OlehHomeWork#6.js
+++ Node.JS/OlehHomeWork#6.js
@@ -1,8 +1,8 @@
 // 1. Сума всіх чисел
-// function sumaGausa(n) {
-//     return (n * (n + 1)) / 2;
-// }
-// console.log(sumaGausa(4));
+function sumaGausa(n) {
+    return (n * (n + 1)) / 2;
+}
+console.log(sumaGausa(4));
 
 
 // 2. Видалення першої та останньої букви із рядку
@@ -18,10 +18,10 @@
 // console.log(StringCut("Ну що тут скажеш"));
 
 // третій варіант
-// function StringCut(s) {
-//     return s.slice(1, -1);
-// }
-// console.log(StringCut("Ну що тут скажеш"));
+function StringCut(s) {
+    return s.slice(1, -1);
+}
+console.log(StringCut("Ну що тут скажеш"));
 
 
 // 3. Розгорнута сума числа
@@ -48,6 +48,7 @@
 //         return result;
 //     }
 // }
+// console.log(expandedForm(-42));
 
 // Краще рішення
 function RozhornytaSuma(num) {
@@ -63,3 +64,49 @@ function RozhornytaSuma(num) {
     return result;
 }
 console.log(RozhornytaSuma(-42));
+
+
+// 4. Сума масиву чисел, окрім найменшого та найбільшого числа
+// Хід думок =)
+// var numbers = [6, 2, 1, 8, 10];
+// numbers.sort(function(a, b) { // сортування
+//     return a - b;
+// });
+// console.log(numbers);
+// Cut = numbers;
+// function StringCut(s) { // обрізання
+//     return s.slice(1, -1);
+// };
+// console.log(StringCut(Cut));
+// numb = StringCut(Cut);
+// var Suma = numb.reduce(function(sum, current) { // додавання
+//     return sum + current
+// });
+// console.log(Suma);
+// var unique = arr.filter(function(elem, index, self) {
+//     return index === self.indexOf(elem);
+// })
+
+// Із дебагом кожної операції
+// function SumCutMaxMin(Array){
+//     console.log(Array);
+//     var Sort = Array.sort(function(a, b) {return a - b});
+//     console.log(Sort);
+//     var Unique = Sort.filter(function(elem, index, self) {return index === self.indexOf(elem)});
+//     console.log(Unique);
+//     var StringCut = Unique.slice(1, -1);
+//     console.log(StringCut);
+//     var Suma = StringCut.reduce((sum, current) => (sum += current), 0);
+//     return (Suma);
+// }
+// console.log(SumCutMaxMin([6, 2, 1, 8, 10, 1]));
+
+// Чисте рішення
+function SumCutMaxMin(Array){
+    var Sort = Array.sort(function(a, b) {return a - b});
+    var Unique = Sort.filter(function(elem, index, self) {return index === self.indexOf(elem)});
+    var StringCut = Unique.slice(1, -1);
+    var Suma = StringCut.reduce((sum, current) => (sum += current), 0);
+    return (Suma);
+}
+console.log(SumCutMaxMin([6, 2, 1, 8, 10, 1]));
