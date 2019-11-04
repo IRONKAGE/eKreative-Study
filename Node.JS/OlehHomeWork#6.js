2d1c05b4b1e8817bc3461d6e0c810e8666f25864
Oleh Hatsenko <40500897+IRONKAGE@users.noreply.github.com>
2019-11-04 13:12:14 +0200

    Add files via upload
    добавлено рішення 6.3

--- Node.JS/OlehHomeWork#6.js
+++ Node.JS/OlehHomeWork#6.js
@@ -1,8 +1,8 @@
 // 1. Сума всіх чисел
-function sumaGausa(n) {
-    return n * (n + 1) / 2;
-}
-console.log(sumaGausa(8));
+// function sumaGausa(n) {
+//     return (n * (n + 1)) / 2;
+// }
+// console.log(sumaGausa(4));
 
 
 // 2. Видалення першої та останньої букви із рядку
@@ -18,10 +18,48 @@ console.log(sumaGausa(8));
 // console.log(StringCut("Ну що тут скажеш"));
 
 // третій варіант
-function StringCut(s) {
-    return s.slice(1, -1);
-}
-console.log(StringCut("Ну що тут скажеш"));
+// function StringCut(s) {
+//     return s.slice(1, -1);
+// }
+// console.log(StringCut("Ну що тут скажеш"));
 
 
 // 3. Розгорнута сума числа
+// Не працює з мінусовими числами
+// function expandedForm(num) {
+//     var i,
+//         position,
+//         numArr = Array.from(num.toString()).map(Number),
+//         numArrLen = numArr.length,
+//         result = '';
+//     if (num < 10) {
+//         return num;
+//     } else {
+//         for(i = 0; i < numArrLen; i++) {
+//         position = numArrLen - 1 - i;
+//         if( i === numArrLen-1 ) {
+//             result += numArr[i] * Math.pow(10, position);
+//         } else {
+//             if(numArr[i] !== 0){
+//             result += numArr[i] * Math.pow(10, position) + " + ";
+//             } 
+//         }
+//         }
+//         return result;
+//     }
+// }
+
+// Краще рішення
+function RozhornytaSuma(num) {
+    let numStringArray = Array.from(String(num));
+    let len = numStringArray.length;
+    let result = '';
+    numStringArray.map((n, index) => {
+        if(n > 0) {
+        if(result) {result += ' + '};
+        result += new String(n).padEnd(len - index, '0');
+        }
+    });
+    return result;
+}
+console.log(RozhornytaSuma(-42));
