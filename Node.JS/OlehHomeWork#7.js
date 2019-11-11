c4a595733abd521ec1975dd047d8fcc0e642f931
Oleh Hatsenko <40500897+IRONKAGE@users.noreply.github.com>
2019-11-11 15:53:15 +0200

    Add files via upload
    Виконав всі завдання 7 уроку

--- Node.JS/OlehHomeWork#7.js
+++ Node.JS/OlehHomeWork#7.js
@@ -1,17 +1,17 @@
-// 1 - Дано три числа a, b, c. Написати функцію яка повертає true, якщо можна побудувати трикутник зі сторін, довжини яких = a, b, c. false - в іншому випадку.
+// 1. Дано три числа a, b, c. Написати функцію яка повертає true, якщо можна побудувати трикутник зі сторін, довжини яких = a, b, c. false - в іншому випадку.
 // Простий варіант
-function trukytnuk(a, b, c){
-    hipotenusa = (c * 2);
-    katet = ((a * 2) + (b * 2));
-    if ((hipotenusa == katet) || (a & b == c)){
-        return true;
-    }else{
-        return false;
-    };
-};
-console.log(trukytnuk(1, 1, 35));
-console.log(trukytnuk(1, 1, 2));
-console.log(trukytnuk(3, 3, 3));
+// function trukytnuk(a, b, c){
+//     hipotenusa = (c * 2);
+//     katet = ((a * 2) + (b * 2));
+//     if ((hipotenusa == katet) || (a & b == c)){
+//         return true;
+//     }else{
+//         return false;
+//     };
+// };
+// console.log(trukytnuk(1, 1, 35));
+// console.log(trukytnuk(1, 1, 2));
+// console.log(trukytnuk(3, 3, 3));
 
 // Математичний варіант
 function trukytnuk(a, b, c){
@@ -21,12 +21,29 @@ function trukytnuk(a, b, c){
         return false;
     };
 };
+console.log(trukytnuk(1, 1, 35));
 console.log(trukytnuk(1, 1, 2));
+console.log(trukytnuk(3, 3, 3));
 
-// 2 - Дане число, функція має повернути середню букву слова - якщо довжина слова непарна і дві середні букви якщо довжина слова - парна.
-// getMiddle("test") -> "es"
-// getMiddle("testing") -> "t"
-// getMiddle("middle") -> "dd"
-// getMiddle("A") -> "A"
-// getMiddle("") -> ""
 
+
+// 2. Дане число, функція має повернути середню букву слова - якщо довжина слова непарна, і дві середні букви якщо довжина слова - парна.
+function Powernennya(text) {
+    var middle;
+    var length;
+    if(text.length % 2 == 1) {
+        middle = text.length / 2;
+        length = 1;
+    } else {
+        middle = text.length / 2 - 1;
+        length = 2;
+    }
+    return text.substring(middle, middle + length)
+}
+
+console.log(Powernennya("Мойсей"));
+console.log(Powernennya("test"));
+console.log(Powernennya("testing"));
+console.log(Powernennya("middle"));
+console.log(Powernennya("A"));
+console.log(Powernennya(""));
