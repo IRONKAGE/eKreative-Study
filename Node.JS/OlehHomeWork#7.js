fe20a1db8f65d12a70ee44e32dacfe238c830483
Oleh Hatsenko <40500897+IRONKAGE@users.noreply.github.com>
2019-11-18 21:01:49 +0200

    Add files via upload

--- Node.JS/OlehHomeWork#7.js
+++ Node.JS/OlehHomeWork#7.js
@@ -1,29 +1,103 @@
 // 1. Дано три числа a, b, c. Написати функцію яка повертає true, якщо можна побудувати трикутник зі сторін, довжини яких = a, b, c. false - в іншому випадку.
 // Простий варіант
+// // function trukytnuk(a, b, c){
+// //     hipotenusa = (c * 2);
+// //     katet = ((a * 2) + (b * 2));
+// //     if ((hipotenusa == katet) || (a & b == c)){
+// //         return true;
+// //     }else{
+// //         return false;
+// //     };
+// // }
+
+
+// Математичний варіант
+// // function trukytnuk(a, b, c){
+// //     if ((c == Math.pow(((a * 2) + (b * 2)), 0.5)) || (a & b == c)){
+// //         return true;
+// //     }else{
+// //         return false;
+// //     };
+// // }
+
+
+// Варіант з матиматиченою формулою, яка включає всі можливі значення
+// // function trukytnuk(a, b, c) {
+// //     if ((a == Math.pow(((b * 2) + (c * 2)), 0.5)) ||
+// //     (b == Math.pow(((a * 2) + (c * 2)), 0.5)) ||
+// //     (c == Math.pow(((a * 2) + (b * 2)), 0.5)) ||
+// //     (a & b == c)){
+// //         return true;
+// //     }if (((a + b) | c) || ((a + c) | b) || ((b + c) | a)){
+// //         return false;
+// //     }else{
+// //         false;
+// //     };
+// // }
+
+
+// Ще один математичний варіант
+// // function trukytnuk(a, b, c) {
+// //     if ((a === Math.pow((Math.pow(b, 2) + Math.pow(c, 2)), 0.5)) ||
+// //     (b === Math.pow((Math.pow(a, 2) + Math.pow(c, 2)), 0.5)) ||
+// //     (c === Math.pow((Math.pow(a, 2) + Math.pow(b, 2)), 0.5)) ||
+// //     (a & b == c) ||
+// //     (a + b > c && a + c > b && b + c > a)){
+// //         return true;
+// //     }else{
+// //         return false;
+// //     }
+// // }
+
+
+// Ще один математичний варіант
+// // function trukytnuk(a, b, c) {
+// //     const ALPHA = parseFloat((Math.acos(((Math.pow(b, 2)) + (Math.pow(c, 2)) - (Math.pow(a, 2))) / (2 * b * c))).toFixed(99));
+// //     const BETTA = parseFloat((Math.acos(((Math.pow(a, 2)) + (Math.pow(c, 2)) - (Math.pow(b, 2))) / (2 * a * c))).toFixed(99));
+// //     const GAMMA = parseFloat((Math.acos(((Math.pow(a, 2)) + (Math.pow(b, 2)) - (Math.pow(c, 2))) / (2 * a * b))).toFixed(99));
+// //     if ((Math.pow(a, 2) == Math.pow(b, 2) + Math.pow(c, 2) - (2 * b * c * Math.cos(ALPHA))) ||
+// //     (Math.pow(b, 2) == Math.pow(a, 2) + Math.pow(c, 2) - (2 * a * c * Math.cos(BETTA))) ||
+// //     (Math.pow(c, 2) == Math.pow(a, 2) + Math.pow(b, 2) - (2 * a * b * Math.cos(GAMMA)))){
+// //         return true;
+// //     }else{
+// //         return false;
+// //     }
+// // }
+
+
+// І ще один математичний варіант
+// // function trukytnuk(a, b, c) {
+// //     const ALPHA = parseFloat((Math.acos(((Math.pow(b, 2)) + (Math.pow(c, 2)) - (Math.pow(a, 2))) / (2 * b * c))).toFixed(99));
+// //     const BETTA = parseFloat((Math.acos(((Math.pow(a, 2)) + (Math.pow(c, 2)) - (Math.pow(b, 2))) / (2 * a * c))).toFixed(99));
+// //     const GAMMA = parseFloat((Math.acos(((Math.pow(a, 2)) + (Math.pow(b, 2)) - (Math.pow(c, 2))) / (2 * a * b))).toFixed(99));
+// //     if ((a / (Math.sin(ALPHA))) === (b / (Math.sin(BETTA))) === (c / (Math.sin(GAMMA)))){
+// //         return true;
+// //     }else{
+// //         return false;
+// //     }
+// // }
+
+
+// Варіант Кості
 // function trukytnuk(a, b, c){
-//     hipotenusa = (c * 2);
-//     katet = ((a * 2) + (b * 2));
-//     if ((hipotenusa == katet) || (a & b == c)){
+//     if ((a + b > c) && (a + c > b) && (b + c > a)){
 //         return true;
 //     }else{
 //         return false;
 //     };
-// };
-// console.log(trukytnuk(1, 1, 35));
-// console.log(trukytnuk(1, 1, 2));
-// console.log(trukytnuk(3, 3, 3));
+// }
 
-// Математичний варіант
+
+// Профі варіант
 function trukytnuk(a, b, c){
-    if ((c == Math.pow(((a * 2) + (b * 2)), 0.5)) || (a & b == c)){
-        return true;
-    }else{
-        return false;
-    };
+        return (a + b > c) && (a + c > b) && (c + b > a);
 }
-console.log(trukytnuk(1, 1, 35));
-console.log(trukytnuk(1, 1, 2));
+
+console.log(trukytnuk(1, 1, 1));
 console.log(trukytnuk(3, 3, 3));
+console.log(trukytnuk(1, 1, 2));
+console.log(trukytnuk(3, 3, 4));
+console.log(trukytnuk(3, 7, 4));
 
 
 
--- Node.JS/OlehHomeWork#8.js
+++ Node.JS/OlehHomeWork#8.js
@@ -1,101 +1,101 @@
-// 1. Повертає Правду або Брехню у булевому значенні
-// function boolToWord( bool ){
-//     if (bool == true){
-//         return 'Yes';
-//     }else{
-//         return 'No';
-//     };
-// }
+// 1. Convert boolean values to strings 'Yes' or 'No' {8kyu} - Повертає Правду або Брехню у булевому значенні
+function boolToWord( bool ){
+    if (bool == true){
+        return 'Yes';
+    }else{
+        return 'No';
+    };
+}
 
-// console.log(boolToWord(true)) == 'Yes';
-// console.log(boolToWord(false)) == 'No';
-
-
-
-// 2. Повернення найбільшого і найменшого числа у рядку
-// function highAndLow(NumStr){
-//     var CreateArr = NumStr.split(" ");
-//     var Max = CreateArr.reduce(function(a, b) {return Math.max(a, b)});
-//     var Min = CreateArr.reduce(function(a, b) {return Math.min(a, b)});
-//     return Max + " " + Min;
-// }
-
-// console.log(highAndLow("1 2 3 4 5")) == "5 1"
-// console.log(highAndLow("1 9 3 4 -5")) == "9 -5";
+console.log(boolToWord(true)) == 'Yes';
+console.log(boolToWord(false)) == 'No';
 
 
 
-// 3. Кількість позитивних та сума негативних чисел
-// function countPositivesSumNegatives(Numbers){
-//     if (!Numbers || !Numbers.length) {return []};
-//     var Positives = Numbers.filter(x => x > 0);
-//     var Negatives = Numbers.filter(x => x <= 0);
-//     return [Positives.length, Math.floor(Negatives.reduce((a, b) => a + b, 0))];
-// }
+// 2. Highest and Lowest {7kyu} - Повернення найбільшого і найменшого числа у рядку
+function highAndLow(NumStr){
+    var CreateArr = NumStr.split(" ");
+    var Max = CreateArr.reduce(function(a, b) {return Math.max(a, b)});
+    var Min = CreateArr.reduce(function(a, b) {return Math.min(a, b)});
+    return Max + " " + Min;
+}
 
-// console.log(countPositivesSumNegatives([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15])) == "10, -65";
+console.log(highAndLow("1 2 3 4 5")) == "5 1"
+console.log(highAndLow("1 9 3 4 -5")) == "9 -5";
+
+
+
+// 3. Count of positives / sum of negatives {8kyu} - Кількість позитивних та сума негативних чисел
+function countPositivesSumNegatives(Numbers){
+    if (!Numbers || !Numbers.length) {return []};
+    var Positives = Numbers.filter(x => x > 0);
+    var Negatives = Numbers.filter(x => x <= 0);
+    return [Positives.length, Math.floor(Negatives.reduce((a, b) => a + b, 0))];
+}
+
+console.log(countPositivesSumNegatives([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15])) == "10, -65";
 
 
 
 // 4. Multiply {8kyu} - Перемножити два числа
-// function multiply(a, b){
-//     return a * b;
-// }
+function multiply(a, b){
+    return a * b;
+}
 
-// console.log(multiply(2, 5)) == "10";
+console.log(multiply(2, 5)) == "10";
 
 
 
 // 5. Even or Odd {8kyu} - Повертає Even для парних чисел, та Odd для непарних чисел
-// function even_or_odd(number) {
-//     if (number % 2 == 0) {
-//         return "Even";
-//     }else{
-//         return "Odd";
-//     };
-// }
+function even_or_odd(number) {
+    if (number % 2 == 0) {
+        return "Even";
+    }else{
+        return "Odd";
+    };
+}
 
-// console.log(even_or_odd(5)) == "Odd";
-// console.log(even_or_odd(8)) == "Even";
+console.log(even_or_odd(5)) == "Odd";
+console.log(even_or_odd(8)) == "Even";
 
 
 
 // 6. Sum of positive {8kyu} - Сумує позитивні числа
-// function positiveSum(arrPositives){
-//     return (arrPositives.reduce((a, b) => a + (b > 0 ? b : 0), 0));
-// }
+function positiveSum(arrPositives){
+    return (arrPositives.reduce((a, b) => a + (b > 0 ? b : 0), 0));
+}
 
-// console.log(positiveSum([1,-4,7,12])) == "1 + 7 + 12 = 20";
+console.log(positiveSum([1,-4,7,12])) == "1 + 7 + 12 = 20";
 
 
 
 // 7. Opposite number {8kyu} - Повертає протилежне число
-// function opposite(number){
-//     return -number;
-// }
+function opposite(number){
+    return -number;
+}
 
-// console.log(opposite(1)) == "-1";
-// console.log(opposite(-9)) == "9";
+console.log(opposite(1)) == "-1";
+console.log(opposite(-9)) == "9";
 
 
 
 // 8. Remove First and Last Character {8kyu} - Видаляє перший та останній символ із рядка
-// function removeChar(strVsjakogo){
-//     return strVsjakogo.slice(1, -1);
-// }
+function removeChar(strVsjakogo){
+    return strVsjakogo.slice(1, -1);
+}
 
-// console.log(removeChar('eloquent')) == "loquen";
-// console.log(removeChar('person')) == "erso";
+console.log(removeChar('eloquent')) == "loquen";
+console.log(removeChar('person')) == "erso";
 
 
 
 // 9. String repeat {8kyu} - Повторює рядок задану кількість разів
-// function repeatStr(count, string){
-//     return string.repeat(count);
-// }
+function repeatStr(count, string){
+    return string.repeat(count);
+}
 
-// console.log(repeatStr(6, "I")) == "IIIIII";
-// console.log(repeatStr(2, "Hi ")) == "Hi Hi ";
+console.log(repeatStr(6, "I")) == "IIIIII";
+console.log(repeatStr(2, "Hi ")) == "Hi Hi ";
 
 
 
